74938f535da4259df5b4b276df9de7e248288838
sergeyg gaan <sergeygaan@mail.ru>
2018-05-23 20:42:36 +0700

    Добавлен проект в ветку

---
+++ MainForm.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace DefinitioEyes
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.imageBoxFrameGrabber = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.button1.Location = new System.Drawing.Point(113, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 35);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Eye detection";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Detection);
+            // 
+            // imageBoxFrameGrabber
+            // 
+            this.imageBoxFrameGrabber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBoxFrameGrabber.Location = new System.Drawing.Point(12, 12);
+            this.imageBoxFrameGrabber.Name = "imageBoxFrameGrabber";
+            this.imageBoxFrameGrabber.Size = new System.Drawing.Size(320, 240);
+            this.imageBoxFrameGrabber.TabIndex = 4;
+            this.imageBoxFrameGrabber.TabStop = false;
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 304);
+            this.Controls.Add(this.imageBoxFrameGrabber);
+            this.Controls.Add(this.button1);
+            this.Name = "FrmPrincipal";
+            this.Text = "Eye detection";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxFrameGrabber)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Emgu.CV.UI.ImageBox imageBoxFrameGrabber;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using System.IO;
+using System.Diagnostics;
+
+namespace DefinitioEyes
+{
+    public partial class FrmPrincipal : Form
+    {
+        //Объявление всех переменных, векторов и хааркаскадов
+        private Image<Bgr, Byte> _currentFrame;
+        private Capture _capture;
+        private HaarCascade _eyes;
+
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+
+            //Загрузка HaarCascade для обнаружения лиц
+            _eyes = new HaarCascade("haarcascade_eye.xml");
+        }
+
+
+        private void Detection(object sender, EventArgs e)
+        {
+            //Инициализация камеры
+            _capture = new Capture();
+            _capture.QueryFrame();
+
+            //Инициализация события  FrameGrabber
+            Application.Idle += new EventHandler(FrameHandler);
+            button1.Enabled = false;
+        }
+
+        private void FrameHandler(object sender, EventArgs e)
+        {
+            //Получение текущего изображение с камеры
+            _currentFrame = _capture.QueryFrame().Resize(320, 240, Emgu.CV.CvEnum.INTER.CV_INTER_CUBIC);
+
+            //Преобразование в серые цвета
+            Image<Gray, byte> _gray = _currentFrame.Convert<Gray, Byte>();
+
+            //Распознание лиц
+            MCvAvgComp[][] facesDetected = _gray.DetectHaarCascade(
+                _eyes,
+                1.2,
+                5,
+                Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.DO_CANNY_PRUNING,
+                new Size(20, 20));
+
+            //Действие для каждого обнаруженного элемента
+            foreach (MCvAvgComp CurrentFace in facesDetected[0])
+            {
+                //Выделение найденного лица
+                _currentFrame.Draw(CurrentFace.rect, new Bgr(Color.Red), 1);
+            }
+
+            //Вывод результата
+            imageBoxFrameGrabber.Image = _currentFrame;
+        }
+    }
+}
